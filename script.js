2c133e011c9cf4e0b505f3152495383c04f0f66e
RealTinyWarrior <sincpls@gmail.com>
2023-08-31 16:53:19 +0600

    Deploy success

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 const route = window.location.href;
-
-if (route === "https://tinytweaks.github.io/") window.location.href = "https://tinytweaks.vercel.app/";
-else {
-    const url = window.location.href;
-    const param = url.split("/")[3];
-
-    window.location.href = `https://${param}.tinytweaks.vercel.app/`;
-}
