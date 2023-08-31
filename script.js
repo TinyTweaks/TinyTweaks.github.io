a57e9c1b71e43167c48554a12809208f80046664
RealTinyWarrior <sincpls@gmail.com>
2023-08-31 23:03:30 +0600

    Fixing redirect bugs

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-window.location.href = "https://tinytweaks.vercel.app/";
+const route = window.location.href;
+
+if (route === "https://tinytweaks.github.io/") window.location.href = "https://tinytweaks.vercel.app/";
+else {
+    const url = window.location.href;
+    const param = url.split("/")[3];
+
+    window.location.href = `https://${param}.tinytweaks.vercel.app/`;
+}
